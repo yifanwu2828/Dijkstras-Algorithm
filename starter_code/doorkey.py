15267cf886ea7d110beba55fc63595742621c5d6
yifanwu2828 <yiw084@ucsd.edu>
2021-04-20 02:02:31 -0400

    finished

--- starter_code/doorkey.py
+++ starter_code/doorkey.py
@@ -1,5 +1,6 @@
 import argparse
-import os.path
+import sys
+import os
 from typing import Union, List
 from collections import namedtuple
 import random
@@ -9,11 +10,11 @@ import numpy as np
 import gym
 from gym_minigrid import minigrid
 from gym_minigrid.minigrid import MiniGridEnv, Door, Wall, Key, Goal
+
 try:
     from icecream import ic
 except ImportError:  # Graceful fallback if IceCream isn't installed.
     ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
-
 
 import utils
 
@@ -88,7 +89,6 @@ def doorkey_partA(env: MiniGridEnv, info: dict, verbose=False):
 
     # Start to Goal
     dist_from_start, prev_start = utils.dijkstra(s=init_agent_pos, grid=binary_grid_carried, direction=init_agent_dir)
-
     path_recon_start2goal = utils.find_shortest_path(
         s=init_agent_pos,
         e=goal_pos,
@@ -99,8 +99,10 @@ def doorkey_partA(env: MiniGridEnv, info: dict, verbose=False):
     act_seq_start2goal, dir_seq_start2goal, cost_seq_start2goal = utils.action_recon(path=path_recon_start2goal,
                                                                                      init_dir=init_agent_dir)
     act_name_start2goal = [inv_act_dict[i] for i in act_seq_start2goal]
-
-    cost_start2goal = sum(cost_seq_start2goal)
+    if not cost_seq_start2goal:
+        cost_start2goal = np.inf
+    else:
+        cost_start2goal = sum(cost_seq_start2goal)
 
     ####################################
     ####################################
@@ -158,36 +160,22 @@ def doorkey_partA(env: MiniGridEnv, info: dict, verbose=False):
 
     ####################################
     ####################################
-    # ic(dist_from_start)
-    # ic(dist_from_key)
-    # ic(dist_from_door)
-
-    start2goal = dist_from_start[tuple(goal_pos)]
-    start2key = dist_from_start[tuple(key_pos)]
-    key2door = dist_from_key[tuple(door_pos)]
-    door2goal = dist_from_door[tuple(goal_pos)]
-
-    start2key2door2goal = np.sum([start2key, key2door, door2goal], dtype=np.float32)
-    # ic(start2goal)
-    # ic(start2key)
-    # ic(key2door)
-    # ic(door2goal)
-    # ic(start2key2door2goal)
+    cost_start2key2door2goal = np.sum([cost_start2key, cost_key2door, cost_door2goal], dtype=np.float32)
 
     opt_act_seq: List = []
     opt_act_name: List = []
 
     # if direct path exist and cost is less than find key
-    if start2goal < start2key2door2goal <= np.inf:
+    if cost_start2goal < cost_start2key2door2goal <= np.inf:
         # Direct to goal
-        print("Direct to Goal is Bette!")
+        print("Direct to Goal is Better!")
         opt_act_seq = act_seq_start2goal
         opt_act_name = act_name_start2goal
 
     # Compare the cost between (start -> goal) and (start -> key -> door -> goal)
     # Or, Direct Path DNE, find key
-    elif start2key2door2goal < start2goal <= np.inf:
-        if start2goal < np.inf:
+    elif cost_start2key2door2goal < cost_start2goal <= np.inf:
+        if cost_start2goal < np.inf:
             print("Find Key is Better!")
         else:
             print("Find Key is the only way!")
@@ -203,79 +191,7 @@ def doorkey_partA(env: MiniGridEnv, info: dict, verbose=False):
     return opt_act_seq, opt_act_name
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--folder", help="Env folder", type=str, default='./envs')
-    parser.add_argument("--seed", help="Random generator seed", type=int, default=42)
-    parser.add_argument("-t", "--test", action="store_true", default=True, help="Test mode")
-    parser.add_argument("--logdir", type=str, default="./gif", help="Log directory")
-    parser.add_argument(
-        "-verb", "--verbose", action="store_true", default=True,
-        help="Verbose mode (False: no output, True: INFO)"
-    )
-    args = parser.parse_args()
-
-    ############################
-    # Config
-    A = False
-    A = True
-    TEST = args.test
-    VERBOSE = args.verbose
-    # VERBOSE = False
-    seed = args.seed
-    env_folder = args.folder
-    logdir = args.logdir
-    ############################
-    if VERBOSE:
-        print(np.__version__)
-    if TEST:
-        # set overflow warning to error instead
-        np.seterr(all='raise')
-    ############################
-    # Seed python and numpy RNG
-    random.seed(seed)
-    np.random.seed(seed)
-    ############################
-    # Part A
-    if A:
-        # Obtain env path
-        env_dict, env_name = utils.fetch_env_dict(env_folder, verbose=VERBOSE)
-        # env_5x5_normal, info_5x5_normal = utils.load_env(env_dict["5x5-normal"])      # pass
-        # env_6x6_direct, info_6x6_direct = utils.load_env(env_dict["6x6-direct"])      # pass
-        # env_6x6_normal, info_6x6_normal = utils.load_env(env_dict["6x6-normal"])      # pass
-        # env_6x6_shortcut, info_6x6_shortcut = utils.load_env(env_dict["6x6-shortcut"])    # pass
-        # env_8x8_direct, info_8x8_direct = utils.load_env(env_dict["8x8-direct"])      # pass
-        # env_8x8_normal, info_8x8_normal = utils.load_env(env_dict["8x8-normal"])      # pass
-        # env_8x8_shortcut, info_8x8_shortcut = utils.load_env(env_dict["8x8-shortcut"])    # pass
-        env_lst = []
-        for key, value in env_dict.items():
-            env, info = utils.load_env(value)
-            env_lst.append((env, info, key))
-
-        for x in env_lst:
-            env, info, key = x
-            print(f'<=========== {key} =============>')
-            opt_act_seq, opt_act_name = doorkey_partA(env, info, verbose=True)
-            print('<===============================>\n')
-    ############################
-    ############################
-
-    # Part B
-    random_env_folder = os.path.join(env_folder, "random_envs")
-    env, info, env_path = utils.load_random_env(random_env_folder)
-    env.seed = seed
-    print(env_path)
-
-    if VERBOSE:
-        print('\n<=====Environment Info =====>')
-        print(env.mission)
-        pprint(info)  # Map size
-        print('<===========================>')
-        # Visualize the environment
-        utils.plot_env(env)
-    ############################
-    ############################
-
+def doorkey_random_partB(env: MiniGridEnv, info: dict, verbose=False):
     # agent info
     init_agent_pos = np.flip(info["init_agent_pos"])
     init_agent_dir = np.flip(info["init_agent_dir"])
@@ -320,14 +236,12 @@ if __name__ == '__main__':
     binary_grid_one_open[tuple(key_pos)] = 0
     # ic(binary_grid_one_open)
 
-
     # binary_grid with door2 open key used
     binary_grid_two_open = binary_grid.copy()
     binary_grid_two_open[tuple(door_pos1)] = 1
     binary_grid_two_open[tuple(door_pos2)] = 0
     binary_grid_two_open[tuple(key_pos)] = 0
     # ic(binary_grid_two_open)
-
 
     # binary_grid with both door open key used
     binary_grid_both_open = binary_grid.copy()
@@ -349,35 +263,259 @@ if __name__ == '__main__':
                                                                                      init_dir=init_agent_dir)
     act_name_start2goal = [inv_act_dict[i] for i in act_seq_start2goal]
 
-    assert len(act_seq_start2goal) == len(act_name_start2goal)
-    for p, name in enumerate(act_name_start2goal):
-        assert act_dict[name] == act_seq_start2goal[p]
-    start2goal = dist_from_start[tuple(goal_pos)]
-    ic(path_recon_start2goal)
-    ic(act_seq_start2goal)
-    ic(act_name_start2goal)
-    ic(dir_seq_start2goal)
-    ic(cost_seq_start2goal)
-    ic(sum(cost_seq_start2goal))
+    if not cost_seq_start2goal:
+        cost_start2goal = np.inf
+    else:
+        cost_start2goal = sum(cost_seq_start2goal)
+    # cost_start2goal = dist_from_start[tuple(goal_pos)]
+    # ic(cost_start2goal)
+    #################################################################################################################
+    # Start to Key
+    path_recon_start2key = utils.find_shortest_path(
+        s=init_agent_pos,
+        e=key_pos,
+        grid=binary_grid_carried,
+        direction=init_agent_dir
+    )
+    # Act Seq Start -> Key
+    act_seq_start2key, dir_seq_start2key, cost_seq_start2key = utils.action_recon(path=path_recon_start2key,
 
+                                                                                  init_dir=init_agent_dir)
+    act_seq_start2key.insert(-1, act.PK)
+    dir_seq_start2key.insert(-1, dir_seq_start2key[-1])
+    cost_seq_start2key.insert(-1, 1)
+    act_name_start2key = [inv_act_dict[i] for i in act_seq_start2key]
 
+    start2key = dist_from_start[tuple(key_pos)]
+    cost_start2key = sum(cost_seq_start2key)
+    # ic(cost_start2key)
+    #################################################################################################################
+    # Key to Door 1
+    dist_from_key, prev_key = utils.dijkstra(s=key_pos, grid=binary_grid_one_open, direction=dir_seq_start2key[-1][-1])
+    path_recon_key2door1 = utils.find_shortest_path(
+        s=key_pos,
+        e=door_pos1,
+        grid=binary_grid_one_open,
+        direction=dir_seq_start2key[-1][-1]
+    )
+    # Act Seq Key -> Door1
+    act_seq_key2door1, dir_seq_key2door1, cost_seq_key2door1 = utils.action_recon(path=path_recon_key2door1,
+                                                                                  init_dir=dir_seq_start2key[-1][-1])
 
+    if door1_is_locked:
+        act_seq_key2door1.insert(-1, act.UD)
+        dir_seq_key2door1.insert(-1, dir_seq_key2door1[-1])
+        cost_seq_key2door1.insert(-1, 1)
+    act_name_key2door1 = [inv_act_dict[j] for j in act_seq_key2door1]
+    cost_key2door1 = sum(cost_seq_key2door1)
+    # ic(cost_key2door1)
 
+    # key to door2
+    path_recon_key2door2 = utils.find_shortest_path(
+        s=key_pos,
+        e=door_pos2,
+        grid=binary_grid_two_open,
+        direction=dir_seq_start2key[-1][-1]
+    )
+    # Act Seq Key -> Door2
+    act_seq_key2door2, dir_seq_key2door2, cost_seq_key2door2 = utils.action_recon(path=path_recon_key2door2,
+                                                                                  init_dir=dir_seq_start2key[-1][-1])
+    if door2_is_locked:
+        act_seq_key2door2.insert(-1, act.UD)
+        dir_seq_key2door2.insert(-1, dir_seq_key2door1[-1])
+        cost_seq_key2door2.insert(-1, 1)
+    act_name_key2door2 = [inv_act_dict[j] for j in act_seq_key2door2]
+    cost_key2door2 = sum(cost_seq_key2door2)
+    # ic(cost_key2door2)
+    #################################################################################################################
+
+    # Door1 to Goal
+    dist_from_door1, prev_door1 = utils.dijkstra(s=door_pos1, grid=binary_grid_one_open,
+                                                 direction=dir_seq_key2door1[-1][-1])
+    path_recon_door12goal = utils.find_shortest_path(
+        s=door_pos1,
+        e=goal_pos,
+        grid=binary_grid_one_open,
+        direction=dir_seq_key2door1[-1][-1]
+    )
+    # Act Seq Door1 -> Goal
+    act_seq_door12goal, dir_seq_door12goal, cost_seq_door12goal = utils.action_recon(path=path_recon_door12goal,
+                                                                                     init_dir=dir_seq_key2door1[-1][-1])
+    act_name_door12goal = [inv_act_dict[k] for k in act_seq_door12goal]
+    cost_door12goal = sum(cost_seq_door12goal)
+    # ic(cost_door12goal)
+
+    # Door2 to Goal
+    dist_from_door2, prev_door2 = utils.dijkstra(s=door_pos2, grid=binary_grid_two_open,
+                                                 direction=dir_seq_key2door2[-1][-1])
+    path_recon_door22goal = utils.find_shortest_path(
+        s=door_pos2,
+        e=goal_pos,
+        grid=binary_grid_two_open,
+        direction=dir_seq_key2door2[-1][-1]
+    )
+    # Act Seq Door1 -> Goal
+    act_seq_door22goal, dir_seq_door22goal, cost_seq_door22goal = utils.action_recon(path=path_recon_door22goal,
+                                                                                     init_dir=dir_seq_key2door2[-1][-1])
+    act_name_door22goal = [inv_act_dict[k] for k in act_seq_door22goal]
+    cost_door22goal = sum(cost_seq_door22goal)
+    # ic(cost_door22goal)
+
+    through_door1 = cost_start2key + cost_key2door1 + cost_door12goal
+    through_door2 = cost_start2key + cost_key2door1 + cost_door22goal
+
+    ic(cost_start2goal)
+    ic(through_door1)
+    ic(through_door2)
+
+    opt_act_seq = []
+    opt_act_name = []
     # Both Door Open
     if door1_is_locked == 0 and door2_is_locked == 0:
         # Go direct to Goal
-        pass
+        print("Both Door Open. Direct to Goal is Better!")
+        opt_act_seq = act_seq_start2goal
+        opt_act_name = act_name_start2goal
+
     # One Door(1) open
     elif door1_is_locked == 0 and door2_is_locked == 1:
         # Door 1 is open and Door 2 is locked
         # Either find key and to door 2 or direct to goal
-        pass
+        if through_door2 < cost_start2goal <= np.inf:
+            print("Find Key and go through Door 2 is Better!")
+            comb_lst = [act_seq_start2key, act_seq_key2door2, act_seq_door22goal]
+            for acs in comb_lst:
+                opt_act_seq.extend(acs)
+            opt_act_name.extend([act_name_start2key, act_name_key2door2, act_name_door22goal])
+
+        elif cost_start2goal < through_door2 <= np.inf:
+            # Go direct to Goal
+            print("Door 1 Open. Direct to Goal is Better!")
+            opt_act_seq = act_seq_start2goal
+            opt_act_name = act_name_start2goal
+
+
     # One Door(2) open
     elif door1_is_locked == 1 and door2_is_locked == 0:
         # Door 2 is open and Door 1 is locked
         # Either find key and to door 1 or direct to goal
-        pass
+        if through_door1 < cost_start2goal <= np.inf:
+            comb_lst = [act_seq_start2key, act_seq_key2door1, act_seq_door12goal]
+            for acs in comb_lst:
+                opt_act_seq.extend(acs)
+            opt_act_name.extend([act_name_start2key, act_name_key2door1, act_name_door12goal])
+
+        elif cost_start2goal < through_door1 <= np.inf:
+            # Go direct to Goal
+            print("Door 2 Open. Direct to Goal is Better!")
+            opt_act_seq = act_seq_start2goal
+            opt_act_name = act_name_start2goal
     # Both Door locked
     else:
         # Find key first, decide to go through door 1 or door 2
-        pass
+        print("Both Door Locked. Find Key is the only way!")
+        if through_door1 < through_door2:
+            print("Through Door 1")
+            comb_lst = [act_seq_start2key, act_seq_key2door1, act_seq_door12goal]
+            for acs in comb_lst:
+                opt_act_seq.extend(acs)
+            opt_act_name.extend([act_name_start2key, act_name_key2door1, act_name_door12goal])
+        else:
+            print("Through Door 2")
+            comb_lst = [act_seq_start2key, act_seq_key2door2, act_seq_door22goal]
+            for acs in comb_lst:
+                opt_act_seq.extend(acs)
+            opt_act_name.extend([act_name_start2key, act_name_key2door2, act_name_door22goal])
+    if verbose:
+        print(f"opt_act_seq: {opt_act_seq}")
+        print(f"opt_act_name: {opt_act_name}")
+    return opt_act_seq, opt_act_name
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--folder", help="Env folder", type=str, default='./envs')
+    parser.add_argument("--seed", help="Random generator seed", type=int, default=42)
+    parser.add_argument("-t", "--test", action="store_true", default=True, help="Test mode")
+    parser.add_argument("--logdir", type=str, default="./gif", help="Log directory")
+    parser.add_argument(
+        "-verb", "--verbose", action="store_true", default=True,
+        help="Verbose mode (False: no output, True: INFO)"
+    )
+    args = parser.parse_args()
+
+    ############################
+    # Config
+    A = False
+    # A = True
+    TEST = args.test
+    VERBOSE = args.verbose
+    # VERBOSE = False
+    seed = args.seed
+    env_folder = args.folder
+    logdir = args.logdir
+    ############################
+    if VERBOSE:
+        print(np.__version__)
+    if TEST:
+        # set overflow warning to error instead
+        np.seterr(all='raise')
+    ############################
+    # Seed python and numpy RNG
+    # random.seed(seed)
+    # np.random.seed(seed)
+    ############################
+    # Part A
+    if A:
+        # Obtain env path
+        env_dict, env_name = utils.fetch_env_dict(env_folder, verbose=VERBOSE)
+        # env_5x5_normal, info_5x5_normal = utils.load_env(env_dict["5x5-normal"])      # pass
+        # env_6x6_direct, info_6x6_direct = utils.load_env(env_dict["6x6-direct"])      # pass
+        # env_6x6_normal, info_6x6_normal = utils.load_env(env_dict["6x6-normal"])      # pass
+        # env_6x6_shortcut, info_6x6_shortcut = utils.load_env(env_dict["6x6-shortcut"])    # pass
+        # env_8x8_direct, info_8x8_direct = utils.load_env(env_dict["8x8-direct"])      # pass
+        # env_8x8_normal, info_8x8_normal = utils.load_env(env_dict["8x8-normal"])      # pass
+        # env_8x8_shortcut, info_8x8_shortcut = utils.load_env(env_dict["8x8-shortcut"])    # pass
+        env_lst = []
+        for key, value in env_dict.items():
+            env, info = utils.load_env(value)
+            env_lst.append((env, info, key))
+
+        for x in env_lst:
+            env, info, key = x
+            print(f'<=========== {key} =============>')
+            opt_act_seq, opt_act_name = doorkey_partA(env, info, verbose=True)
+            for ac in opt_act_seq:
+                try:
+                    utils.step(env, ac, render=True)
+                except KeyboardInterrupt:
+                    sys.exit(0)
+            print('<===============================>\n')
+    ############################
+    ############################
+
+    # Part B
+    random_env_folder = os.path.join(env_folder, "random_envs")
+    for i in range(30):
+        env, info, env_path = utils.load_random_env(random_env_folder)
+        # env.seed = seed
+        print(env_path)
+
+        if VERBOSE:
+            print('\n<=====Environment Info =====>')
+            print(env.mission)
+            pprint(info)  # Map size
+            print('<===========================>')
+            # Visualize the environment
+            utils.plot_env(env)
+        ############################
+        ############################
+        print(f'<========================>')
+        opt_act_seq, opt_act_name = doorkey_random_partB(env, info, verbose=True)
+        for ac in opt_act_seq:
+            try:
+                utils.step(env, ac, render=True)
+            except KeyboardInterrupt:
+                sys.exit(0)
+        print('<===============================>\n')
+
